43a48a83c32e4de07baf21831eeec95c51baff06
Colby Brundin <colbybrundin@Colbys-MacBook-Air.local>
2018-01-27 15:57:33 -0500

    Lesson-04

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Colby Brundin on 1/27/18.
+//  Copyright © 2018 Colby Brundin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Colby Brundin on 1/27/18.
+//  Copyright © 2018 Colby Brundin. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
